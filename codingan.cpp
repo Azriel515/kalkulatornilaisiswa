eff6be18dff824f51d2b8ceae2a5468543377c25
Azriel515 <azriel.ilham1234@gmail.com>
2024-07-11 18:17:45 +0800

    Menerapkan fungsi rata-rata bobot

--- codingan.cpp
+++ codingan.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
-float hitungNilaiAkhir(Siswa siswa) {
-    return (siswa.nilaiTugas * 0.3) + (siswa.nilaiKuis * 0.2) + (siswa.nilaiUjian * 0.5);
+void hitungRataRataBobot(Siswa siswa) {
+    float rataRataBobot = (siswa.nilaiTugas + siswa.nilaiKuis + siswa.nilaiUjian) / 3.0;
+    cout << "Rata-rata bobot " << siswa.nama << " adalah " << rataRataBobot << endl;
 }
